e98402c532b91f1c08be5053f0ade8290a23aef2
ildikowolf <farkas@sakraf.hu>
2025-11-23 21:47:16 +0100

    Absztrakt osztály definiálása

--- Classes/Auto.py
+++ Classes/Auto.py
@@ -1,5 +1,16 @@
-class Auto:
+from abc import ABC, abstractmethod
+
+# Ősosztály
+class Auto(ABC):
     def __init__(self, rendszam, tipus, berleti_dij):
-        self.rendszam = rendszam
-        self.tipus = tipus
-        self.berleti_dij = berleti_dij
+        self._rendszam = rendszam
+        self._tipus = tipus
+        self._berleti_dij = berleti_dij
+
+    @abstractmethod
+    def auto_berles(self):
+        pass
+
+    @abstractmethod
+    def auto_berles_lemondas(self):
+        pass
